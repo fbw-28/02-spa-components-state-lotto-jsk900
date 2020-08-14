4beb9495bd3a189861ff8e5758cfeacb1e63d1cb
jsk900 <jsk900@yahoo.co.uk>
2020-08-14 09:17:37 +0200

    Exercise 02

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
+import React, { Component } from 'react';
+import SingleNumber from './components/SingleNumber';
+import MultiNumbers from './components/MultiNumbers';
 
-import React from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    luckyNumbersArr: [],
+    singleNumber: null,
+  };
+
+  generateRandomNumber = (max) => {
+    return Math.floor(Math.random() * max) + 1;
+  };
+
+  generateLuckyNumbers = () => {
+    this.setState({ singleNumber: this.generateRandomNumber(10) });
+    const tempArray = [];
+    let randomNumber = 0;
+    for (let i = 0; i < 6; i++) {
+      randomNumber = this.generateRandomNumber(49);
+
+      if (tempArray.includes(randomNumber)) {
+        randomNumber = this.generateRandomNumber(49);
+        i--;
+      } else {
+        tempArray.push(randomNumber);
+      }
+    }
+    this.setState({ luckyNumbersArr: tempArray });
+  };
+
+  render() {
+    return (
+      <main>
+        <h1>Lotto 6 / 49</h1>
+        <h4>Generating lucky numbers</h4>
+        <section>
+          {this.state.luckyNumbersArr.map((num) => (
+            <MultiNumbers multiNumber={num} />
+          ))}
+
+          <SingleNumber singleNumber={this.state.singleNumber} />
+        </section>
+        <button
+          onClick={() =>
+            this.setState({ luckyNumbersArr: [], singleNumber: null })
+          }>
+          Reset
+        </button>
+        <button onClick={this.generateLuckyNumbers}>
+          Show me the lucky numbers
+        </button>
+      </main>
+    );
+  }
 }
-
 
 export default App;
---
+++ src/components/SingleNumber.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleNumber = ({ singleNumber }) => {
+  return (
+    <div id='single' className='round'>
+      <p id='singleP'>{singleNumber}</p>
+    </div>
+  );
+};
+
+export default SingleNumber;
